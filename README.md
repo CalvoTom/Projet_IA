6ffbf93a65be654306e6039148a7d3b939b082ec
Tom <131645934+CalvoTom@users.noreply.github.com>
2023-05-06 10:31:47 +0000

    Modification de la fonction transform. 	modified:   README.md 	modified:   transform.py

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Projet_IA
 # V.1.1
 - Ajout de la fonction transform (importer de la V.0)
+- Modification de la fonction transform.
--- transform.py
+++ transform.py
@@ -1,9 +1,20 @@
+from PIL import Image
 import torch
-import torchvision.transforms as transforms
+from torchvision import transforms
 
-def transform_tenseur():
-    transform = transforms.Compose(
-    [transforms.Resize((224, 224)),
-     transforms.ToTensor(),
-     transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                          std=[0.229, 0.224, 0.225])])
+def transform_tenseur(chm_image):
+    """
+    Cette fonction transforme une image en tenseur normalisé. Elle prend en paramètre le chemin vers cette image.
+    """
+    # Ouvrir l'image et vérifier la taille
+    image = Image.open(chm_image)
+    if image.size != (224, 224):
+        image = image.resize((224, 224))
+    
+    # Convertir l'image en mode RVB et appliquer les transformations
+    image = image.convert('RGB')
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+    tensor = transform(image)
+    return tensor
